6072bc3794e9fbc52b84e651bc7739719755c350
iellorin <iellorin@hotmail.com>
2015-01-24 11:59:57 -0800

    New Platforms (sticky, vertical)
    Players now no longer "slide" off of the platforms!
    
    Vertical platforms prefab added

---
+++ Assets/Scripts/HoldPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldPlayer : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			col.transform.parent = gameObject.transform;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			col.transform.parent = null;
+		}
+	}
+}
