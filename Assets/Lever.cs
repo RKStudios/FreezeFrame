85d531bfa932818f19c7edcab891cb838b2a25bc
RKStudios <r_kieu@hotmail.com>
2015-01-24 12:52:21 -0800

    Levering
    levers

---
+++ Assets/Lever.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lever : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+/*		if (other.tag == "Player")
+		{
+			return;
+		}
+*/
+		transform.Rotate(new Vector3(0,0,150) /* * Time.deltaTime */);
+
+	}
+}
