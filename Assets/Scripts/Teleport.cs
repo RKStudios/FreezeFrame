ad2cf76d257ceb6e6c4536bc6836f4a03b4206d7
Hanson Chun <hansonchun@gmail.com>
2015-01-24 13:51:16 -0800

    Added portal
    - portal added
    - triggers by tag “player”

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+	public GameObject target;
+	public float adjustTarget;
+	bool jump;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (!jump) {
+			if (other.gameObject.tag == "Player") {
+					target.GetComponent<Teleport>().jump = true;
+					other.gameObject.transform.position = new Vector3 (target.transform.position.x, 
+			                                                   			target.transform.position.y + adjustTarget, 0);
+					}
+				}
+		}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.tag == "Player") {
+			jump = false;
+		}
+	}
+}
