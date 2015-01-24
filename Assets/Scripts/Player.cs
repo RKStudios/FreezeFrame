7a8ae5920d2c3c9bb62957d606f3e9681720581c
Joseph Choi <robocup30@hotmail.com>
2015-01-24 10:06:08 -0800

    Initial player setup
    Player movement and jump implemented

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	int speed = 1000;
+	int jumpPower = 500;
+	float horizontalMovement = 0;
+	bool isPlayerMoving = false;
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+
+	void FixedUpdate()
+	{
+
+		if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+		{
+			isPlayerMoving = true;
+		}
+
+		horizontalMovement = Input.GetAxis("Horizontal");
+
+		Debug.Log(horizontalMovement);
+		
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			rigidbody2D.AddForce(Vector2.up * jumpPower);
+		}
+
+		rigidbody2D.AddForce(Vector2.right * horizontalMovement * Time.deltaTime * speed);
+
+	}
+
+	// Update is called once per frame
+	void Update() 
+	{
+
+	}
+}
