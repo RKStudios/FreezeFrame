65ef03182576d5dec166b0b3e2d4b0e7c9d2cbda
Johnnie <johnnie-l@live.ca>
2015-01-24 18:53:50 -0800

    Quit testing.

--- Assets/MainMenu/QuitGame.cs
+++ Assets/MainMenu/QuitGame.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class QuitGame : MonoBehaviour {
 	void OnTriggerStay2d(Collider2D other) {
 		if (Input.GetButtonDown ("Action")) {
+			print ("Quit");
 			Application.Quit();
 		}
 	}
