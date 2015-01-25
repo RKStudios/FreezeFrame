0b6b8fdbe4ee02e8042fe655f21418557a482de4
Hanson Chun <hansonchun@gmail.com>
2015-01-24 19:16:54 -0800

    updated Level 4
    - its actually a tutorial level now
    - still need to add “press shift” text

---
+++ Assets/FreezeBox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeBox : MonoBehaviour {
+	public GameObject other;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGui() {
+		GUI.Label (new Rect (10, 10, 100, 20), "Press Shift");
+
+}
+}
