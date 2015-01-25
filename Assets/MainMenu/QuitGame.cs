5067335f6cde52e97ebf5899a61f1a9e9f95d8bb
Johnnie <johnnie-l@live.ca>
2015-01-24 17:07:39 -0800

    Main Menu

---
+++ Assets/MainMenu/QuitGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+	void OnTriggerStay2d(Collider2D other) {
+		if (Input.GetButtonDown ("Action")) {
+			Application.Quit();
+		}
+	}
+}
