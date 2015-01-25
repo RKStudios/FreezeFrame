9d2a815034776b0951d1cb14c9fbd4630f5b69f1
Joseph Choi <robocup30@hotmail.com>
2015-01-24 18:52:37 -0800

    Fixed Jump on platforms

--- Assets/scripts/HoldPlayer.cs
+++ Assets/scripts/HoldPlayer.cs
@@ -5,17 +5,11 @@ public class HoldPlayer : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		if (col.gameObject.tag == "Player")
-		{
-			col.transform.parent = gameObject.transform;
-		}
+
 	}
 
 	void OnTriggerExit2D(Collider2D col)
 	{
-		if (col.gameObject.tag == "Player")
-		{
-			col.transform.parent = null;
-		}
+
 	}
 }
